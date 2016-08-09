5332d9e073f78441dd45ec9bea9f4ef82560f1f1
darkframemaster <1119369256@qq.com>
2016-08-10 01:27:40 +0800

    lecture-four php&xml

---
+++ web_roadmap/lecture_4/blog.php
@@ -0,0 +1,38 @@
+<?php
+	/*
+	* Using function `simplexml_load_file` to read a xml file.
+	* The param can be a filename and a whole URL.
+	* The variable `dom` will point at the root point of the xml datastructure,and in this case it is `blogs`.
+	*/
+	$dom = simplexml_load_file("blogs.xml");
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Blogs</title>
+</head>
+<body>
+	<h1>Blogs</h1>
+	<ul>
+	<?php
+		print($dom);
+		foreach ($dom->blog as $blog)
+		{
+			print("<li>");
+			print($blog["number"]);
+			print(": ");
+			print($blog->title);
+			print("<ul>");
+			foreach ($blog->body as $body)
+			{	
+				print("<li>");
+				print($body);
+				print("</li>");
+			}
+			print("</ul>");
+			print("</li>");
+		}
+	?>
+	</ul>
+</body>
+</html>
